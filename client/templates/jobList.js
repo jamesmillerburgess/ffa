5f4ce45302028417171a411db7344abc7beec963
James Burgess <jamesmillerburgess@gmail.com>
2016-02-27 18:15:47 +0100

    Removed autopublish and added publish/subscribe for Jobs.

--- client/templates/jobList.js
+++ client/templates/jobList.js
@@ -1,9 +1,3 @@
-var jobData = [
-  {title: "India to United Kingdom"},
-  {title: "China to Germany"},
-  {title: "Germany to United States"}
-];
-
 Template.jobList.helpers({
   jobs: function() {
     return Jobs.find();
